a98b6dad4ce9f034381c408e60fc68e4a2ad139e
shavyg2 <shavyg2@gmail.com>
2014-05-05 18:01:30 -0400

    final with the lorem and no images

--- application/app/view/js.php
+++ application/app/view/js.php
@@ -1,3 +1,7 @@
 <?php //ini_set('error_reporting', 0);?>
+<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+<script type="text/javascript" src="<?php echo assets("js/plugin/flux.min.js") ?>"></script>
+<script type="text/javascript" src="<?php echo assets("js/slider.js") ?>"></script>
+
 
 
--- application/app/view/service.php
+++ application/app/view/service.php
@@ -23,45 +23,97 @@
 ?>
 
 <?php ob_start();?>
+
+<div id="slider">
+    <img src="<?php echo assets("image/ac_unit_roof_top.png") ?>" alt=""/>
+    <img src="<?php echo assets("image/drain-camera.png") ?>" alt=""/>
+    <img src="<?php echo assets("image/inspection.png") ?>" alt=""/>
+    <img src="<?php echo assets("image/repair_guest.png") ?>" alt=""/>
+    <img src="<?php echo assets("image/repair_planning.png") ?>" alt=""/>
+</div>
+
 <h1>Services</h1>
-<ul class="spacing">
-    <li><div class="thumbnail" id="plumbing-image">
-        <div class="text">Plumbing Services</div>
-    </div></li>
-    <li><div class="thumbnail" id="gas-image">
-        <div class="text">Gas appliance installation</div>
-    </div></li>
-    <li><div class="thumbnail" id="drain-image">
-        <div class="text">CCTV Drain/Video Inspection</div>
-    </div></li>
-    <li><div class="thumbnail" id="ac-image">
-        <div class="text">Air Condition Repair & Installation</div>
-    </div></li>
-    <li><div class="thumbnail" id="furnace-image">
-        <div class="text">Furnace Repair & Installation</div>
-    </div></li>
-    <li><div class="thumbnail" id="sprinkler-image">
-        <div class="text">Sprinkler System</div>
-    </div></li>
-</ul>
+<!--<ul class="spacing">-->
+    <!--<li>-->
+        <!--<div class="thumbnail" id="plumbing-image">-->
+            <!--<div class="text">Plumbing Services</div>-->
+        <!--</div>-->
+    <!--</li>-->
+    <!--<li>-->
+        <!--<div class="thumbnail" id="gas-image">-->
+            <!--<div class="text">Gas appliance installation</div>-->
+        <!--</div>-->
+    <!--</li>-->
+    <!--<li>-->
+        <!--<div class="thumbnail" id="drain-image">-->
+            <!--<div class="text">CCTV Drain/Video Inspection</div>-->
+        <!--</div>-->
+    <!--</li>-->
+    <!--<li>-->
+        <!--<div class="thumbnail" id="ac-image">-->
+            <!--<div class="text">Air Condition Repair & Installation</div>-->
+        <!--</div>-->
+    <!--</li>-->
+    <!--<li>-->
+        <!--<div class="thumbnail" id="furnace-image">-->
+            <!--<div class="text">Furnace Repair & Installation</div>-->
+        <!--</div>-->
+    <!--</li>-->
+    <!--<li>-->
+        <!--<div class="thumbnail" id="sprinkler-image">-->
+            <!--<div class="text">Sprinkler System</div>-->
+        <!--</div>-->
+    <!--</li>-->
+<!--</ul>-->
 
+<div class="service-information">
+    <div class="information">
+        <h5>Plumbing Services</h5>
+<?php include('plumbing-service.php') ?>
+    </div>
 
-<h1>Service Area</h1>
-<div class="image-map">
-    <div class="map-section" id="peel"></div>
-    <div class="map-section" id="durham"></div>
-    <div class="map-section" id="halton"></div>
-    <div class="map-section" id="toronto"></div>
-    <div class="map-section" id="york"></div>
+    <div class="information">
+        <h5>Gas Appliance Installation</h5>
+<?php include('gas-appliance-installation.php') ?>
+    </div>
+
+    <div class="information last">
+        <h5>CCTV Drain/Video Inspection</h5>
+<?php include('cctv-drain-video-inspection.php') ?>
+    </div>
+
+    <div class="information">
+        <h5>Air Condition Repair & Installation</h5>
+<?php include('air-condition-repair-installation.php') ?>
+    </div>
+
+    <div class="information">
+        <h5>Furnace Repair & Installation</h5>
+<?php include('furnace-repair-installation.php') ?>
+    </div>
+
+    <div class="information">
+        <h5>Sprinkler System</h5>
+<?php include('sprinkler-system.php') ?>
+    </div>
 </div>
 
-<div class="map-labels">
-    <div class="region" id="toronto">Toronto</div>
-    <div class="region" id="york">York</div>
-    <div class="region" id="durham">Durham</div>
-    <div class="region" id="halton">Halton</div>
-    <div class="region" id="peel">Peel</div>
-</div>
+<!--<h1>Service Area</h1>-->
+<!--<div class="image-map">-->
+<!--<div class="map-section" id="peel"></div>-->
+<!--<div class="map-section" id="durham"></div>-->
+<!--<div class="map-section" id="halton"></div>-->
+<!--<div class="map-section" id="toronto"></div>-->
+<!--<div class="map-section" id="york"></div>-->
+<!--</div>-->
+
+<!--<div class="map-labels">-->
+<!--<div class="region" id="toronto">Toronto</div>-->
+<!--<div class="region" id="york">York</div>-->
+<!--<div class="region" id="durham">Durham</div>-->
+<!--<div class="region" id="halton">Halton</div>-->
+<!--<div class="region" id="peel">Peel</div>-->
+<!--</div>-->
 <?php
   $section_buffer=ob_get_clean();
   $last->sections['page']=$section_buffer
