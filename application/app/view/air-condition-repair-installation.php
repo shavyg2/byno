fceddc71a82db2a59946828eb3e93574614589cc
Shavauhn Gabay <shavyg2@gmail.com>
2014-05-19 17:32:26 -0400

    automatic save

--- application/app/view/air-condition-repair-installation.php
+++ application/app/view/air-condition-repair-installation.php
@@ -1,3 +1,3 @@
 <?php //ini_set('error_reporting', 0);?>
-<p>Cool down and enjoy your summer days. Certified technician will install or repair any make or model unit <br>
-        <strong> Commercial Roof Top or Residential Unit</strong></p>
+<p>Cool down and enjoy your summer days. Certified technicians will install or repair any make or model unit <br>
+        <strong> Commercial Industrial or Residential Unit</strong></p>
