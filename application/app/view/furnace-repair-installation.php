97a1830f9ccd5e356a4fdf42c9b39c9cc77641e3
Shavauhn Gabay <shavyg2@gmail.com>
2014-05-24 14:34:44 -0400

    automatic save

--- application/app/view/furnace-repair-installation.php
+++ application/app/view/furnace-repair-installation.php
@@ -1,4 +1,4 @@
 <?php //ini_set('error_reporting', 0);?>
 
 <p>Byno Mechanical is the Company you should call for any furnace need <br>
-We sell install and repair Commercial, Industrial and Residential units.</p>
+We sell, install and repair Commercial, Industrial and Residential units.</p>
